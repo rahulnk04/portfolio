9a18653096ab1705d9ad1d9969daea166450261d
rahulnk04 <rahulnk04@gmail.com>
2026-05-26 16:13:45 +0530

    updated code

--- src/Data/Data.ts
+++ src/Data/Data.ts
@@ -2,6 +2,7 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PinterestIcon from '@mui/icons-material/Pinterest';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import dxcLogo from 'src/images/DXC-Full-Color.png';
 import CssIcon from 'src/images/icons/css.png';
@@ -220,6 +221,11 @@ const Data: ResumeData = {
       name: 'Instagram',
       url: 'https://www.instagram.com/rahulnk04/',
       icon: InstagramIcon,
+    },
+    {
+      name: 'Pinterest',
+      url: 'https://in.pinterest.com/rahulnk04/rahul-ranjan-nayak/',
+      icon: PinterestIcon,
     },
   ],
   profilePicture: Me,
